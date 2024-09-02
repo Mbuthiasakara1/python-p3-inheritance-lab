3a8ba0c289cc8cf27dd00cc94a26e1842c9078a8
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-09-02 23:22:00 +0300

    inheritance lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,23 @@
 from user import User
 
 class Student(User):
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge=[]
+
     
-    def learn(self):
-        pass
+
+    
+    def learn(self,new_string):
+        self.knowledge.append(new_string)
+
+           
+def main():
+    student = Student("jOHN",'DOE')
+    print(student.knowledge)
+    student.learn("Python")
+    student.learn("woii ")
+    print(student.knowledge)
+
+if __name__ == "__main__":
+    main()   
